c58b3b7693f3e070478ebf4b7b60701fb85a47f2
Pai1 <37597520+DeoDorqnt387@users.noreply.github.com>
2024-10-25 23:40:48 +0300

    added: password, confirm, input.

--- src/vin/vin.v
+++ src/vin/vin.v
@@ -1,10 +1,12 @@
 module vin
 
+import os
 import term
 import readline { read_line }
 
 pub struct Question {
     pub:
+    type string
     prompt string
     choices []string
 }
@@ -23,41 +25,70 @@ pub fn display_question(q Question) !string {
 
     for {
         term.set_cursor_position(x: 0, y: 0)
-        println(q.prompt)
-        for i, choice in q.choices {
-            if i == selected_index {
-                println(term.bright_green('> ${choice}'))
-            } else {
-                println('  ${choice}')
-            }
-        }
-
-
-        mut input := []u8{len: 3}
-        input[0] = u8(r.read_char()!) 
-        if input[0] == `\e` {
-            input[1] = u8(r.read_char()!) 
-            input[2] = u8(r.read_char()!)
-        } else if input[0] == `\r` {
-            return q.choices[selected_index]
-        } else if input[0] == `\e` {
-            return error("Escaped")
-        }
-
-        match input.bytestr() {
-            '\x1b[A' { // Up arrow key
-                if selected_index > 0 {
-                    selected_index--
+        if q.type == "list"{
+            println(q.prompt)
+            for i, choice in q.choices {
+                if i == selected_index {
+                    println(term.bright_green('> ${choice}'))
+                } else {
+                    println('  ${choice}')
                 }
             }
-            '\x1b[B' { // Down arrow key
-                if selected_index < q.choices.len - 1 {
-                    selected_index++
-                }
+
+            mut input := []u8{len: 3}
+            input[0] = u8(r.read_char()!) 
+            if input[0] == `\e` {
+                input[1] = u8(r.read_char()!) 
+                input[2] = u8(r.read_char()!)
+            } else if input[0] == `\r` {
+                return q.choices[selected_index]
+            } else if input[0] == `\e` {
+                return error("Escaped")
             }
-            else {}
+
+            match input.bytestr() {
+                '\x1b[A' {
+                    if selected_index > 0 {
+                        selected_index--
+                    }
+                }
+                '\x1b[B' {
+                    if selected_index < q.choices.len - 1 {
+                        selected_index++
+                    }
+                }
+                else {}
+            }
+        } else if q.type == "input"{
+            r.disable_raw_mode()
+            term.show_cursor()
+
+            input := os.input("${q.prompt}: ")
+            return input
+        } else if q.type == "confirm" {
+            r.disable_raw_mode()
+            term.show_cursor()
+
+            mut input := os.input("${q.prompt}:(y/N) ")
+            input = input.to_lower()
+            println(input)
+            if input != "y" && input != "n"{
+                panic("Must be y or N.")
+            }else{
+                return input
+            }
+            
+        } else if q.type == "password" {
+            r.disable_raw_mode()
+            term.show_cursor()
+
+            input := os.input_password("${q.prompt}: ")!
+            return input
+        } else {
+            panic("Unsupported question type: ${q.type}")
+            return ""
         }
-    }
+    } 
+
     return error("Exited loop")
 }
-
